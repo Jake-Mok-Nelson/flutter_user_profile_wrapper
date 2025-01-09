dabfeffbf5f64d55df213bfbfed9a1d99ea31e5d
jake-mok-nelson <Jake-Mok-Nelson@users.noreply.github.com>
2025-01-09 14:25:34 +1100

    generate website

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4254040991"
+    serviceWorkerVersion: "3462258187"
   }
 });
